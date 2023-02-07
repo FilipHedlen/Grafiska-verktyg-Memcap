3047b76ebd4aa544fb159d8463e0d49f076133cd
FilipHedlen <114926854+FilipHedlen@users.noreply.github.com>
2023-02-07 15:54:19 +0100

    .

---
+++ README.md
@@ -0,0 +1,15 @@
+För Godkänt (jämför kriterierna för VG):
+
+    Webbplatsen ska följa designen
+    Webbplatsen ska vara responsiv och fungera oavsett enhet/upplösning
+    Webbplatsen ska följa best practice vad avser HTML/CSS
+    Webbplatsen ska vara publicerad live
+
+För VG:
+
+    Effekter på knappar ska finnas med
+    Koden ska hålla en mycket hög nivå vad avser namngivning, semantik, etc.
+    Webbplatsen skall vara optimerad (Lighthouse-analys)
+    Menyknappen skall vara animerad
+    Menyn ska animeras in
+    Cookie-boxen ska vara semantiskt korrekt strukturerad i HTML så att läsordningen är logisk, och utseendet ska ha fixats med CSS (knapparnas ordning + textens placering), så att om t.ex. CSS tas bort så ligger allt i en logisk ordning.
---
+++ js/main.js
@@ -0,0 +1,36 @@
+const menu = document.querySelector(".nav-menu");
+const menuBtn = document.querySelector(".menuOpener");
+const menuOpener = document.querySelector('.menuOpener');
+const acceptBtn = document.querySelector(".accept");
+const rejectBtn = document.querySelector(".reject");
+const cookiesDiv = document.querySelector(".cookies")
+const darkenDiv = document.querySelector(".darken"); 
+
+// Toggle menu
+menu.style.display = "none";
+menuBtn.addEventListener("click", function() {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+});
+
+// Rotate menu btn
+menuOpener.addEventListener('click', () => {
+menuOpener.classList.toggle('rotate');
+});
+
+// Cookie pop-up
+document.addEventListener("DOMContentLoaded", () => {
+  acceptBtn.addEventListener("click", () => {
+    cookiesDiv.style.display = "none";
+    darkenDiv.style.display = "none"; // Hides the dark div after pop-up is closed
+  });
+
+  rejectBtn.addEventListener("click", () => {
+    cookiesDiv.style.display = "none";
+    darkenDiv.style.display = "none"; // Hides the dark div after pop-up is closed
+  });
+});
+
